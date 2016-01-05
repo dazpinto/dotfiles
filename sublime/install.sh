dd7ae44de9dc03aeabd3a249a0a0fdf60e07f92d
Darryl Pinto <dazpinto@gmail.com>
2016-01-05 14:47:20 +0000

    stuff

--- sublime/install.sh
+++ sublime/install.sh
@@ -5,7 +5,7 @@ IFS=""
 
 mkdir $sublimeDestination
 for file in ${sublimeFiles[*]}; do
-	# mv $sublimeDestination/$file ~/dotfiles_sublime_old/ -f
+	mv $sublimeDestination/$file ~/dotfiles_sublime_old/ -f
 	echo "Symlinking $sublimeDir/$file to $sublimeDestination/$file"
 	ln -s $sublimeDir/$file $sublimeDestination/$file
 done
